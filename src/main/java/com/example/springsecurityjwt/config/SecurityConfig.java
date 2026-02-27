0df1fbcdb5f3f79dbe1798a9d6b28898a5690393
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-02-27 17:59:24 +0000

    feat: make session StateLess

--- src/main/java/com/example/springsecurityjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurityjwt/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,12 +14,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
 
+        // Disable CSRF security
         http.csrf(customize -> customize.disable());
+        // Authorize Request
         http.authorizeHttpRequests(
                 request ->
                         request.anyRequest().authenticated());
+
+        // Show Form Login
         http.formLogin(Customizer.withDefaults());
+        // Allow postman request
         http.httpBasic(Customizer.withDefaults());
+        // Session StateLess
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
 
         return http.build();
     }
