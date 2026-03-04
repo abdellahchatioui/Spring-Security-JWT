ea08b2b14293cb87905959adeb955c1b7ff78999
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-04 12:16:30 +0000

    feat: create our own UserDetaisService

--- src/main/java/com/example/springsecurityjwt/service/MyUserDetailsService.java
+++ src/main/java/com/example/springsecurityjwt/service/MyUserDetailsService.java
@@ -1,4 +1,27 @@
 package com.example.springsecurityjwt.service;
 
-public class MyUserDetailsService {
+import com.example.springsecurityjwt.entity.UserPrincipal;
+import com.example.springsecurityjwt.entity.Users;
+import com.example.springsecurityjwt.repo.UsersRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UsersRepo usersRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = usersRepo.findByUsername(username);
+        if(user == null){
+            System.out.println("user not found");
+            throw new UsernameNotFoundException("user not found");
+        }
+        return new UserPrincipal(user);
+    }
 }
