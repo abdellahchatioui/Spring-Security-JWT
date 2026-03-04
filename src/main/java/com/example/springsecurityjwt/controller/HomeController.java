2797b953bfdbdac4ab1503d469be750b952f59b2
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-04 22:09:29 +0000

    refactor: permite /auth/register request without authentication

--- src/main/java/com/example/springsecurityjwt/controller/HomeController.java
+++ src/main/java/com/example/springsecurityjwt/controller/HomeController.java
@@ -1,12 +1,18 @@
 package com.example.springsecurityjwt.controller;
 
+import com.example.springsecurityjwt.entity.Users;
+import com.example.springsecurityjwt.repo.UsersRepo;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
