482c258632400ae1806d0a7065df78a0840439e3
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-04 22:27:40 +0000

    refactor: encode password before saving in db

--- src/main/java/com/example/springsecurityjwt/service/UserService.java
+++ src/main/java/com/example/springsecurityjwt/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.springsecurityjwt.service;
 import com.example.springsecurityjwt.entity.Users;
 import com.example.springsecurityjwt.repo.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,7 +13,10 @@ public class UserService {
     @Autowired
     UsersRepo usersrepo;
 
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     public Users register(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
         usersrepo.save(user);
         return user;
     }
