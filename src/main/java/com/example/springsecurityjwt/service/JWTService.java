93fcc875a732fbbf05839dc1f7b66322d1e1b102
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-05 23:25:47 +0000

    feat: create JWTService and implement a fake generateToken for testing

--- src/main/java/com/example/springsecurityjwt/service/JWTService.java
+++ src/main/java/com/example/springsecurityjwt/service/JWTService.java
@@ -1,4 +1,11 @@
 package com.example.springsecurityjwt.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class JWTService {
+    public String generateToken() {
+        return "test-token";
+    }
+
 }
