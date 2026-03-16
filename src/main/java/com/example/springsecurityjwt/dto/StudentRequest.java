72ff0ca266251d42f1f0c585616be201e93ad7e4
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-16 21:49:05 +0000

    feat: implement StudentRequest dto

--- src/main/java/com/example/springsecurityjwt/dto/StudentRequest.java
+++ src/main/java/com/example/springsecurityjwt/dto/StudentRequest.java
@@ -1,4 +1,16 @@
 package com.example.springsecurityjwt.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentRequest {
+
+    private String name;
+    private int mark;
+
 }
