379df2a23b1aedbf7483451dc3bd714e7c1b7787
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-16 21:46:59 +0000

    refactor: refactor the HomeController

--- src/main/java/com/example/springsecurityjwt/controller/HomeController.java
+++ src/main/java/com/example/springsecurityjwt/controller/HomeController.java
@@ -1,18 +1,10 @@
 package com.example.springsecurityjwt.controller;
 
-import com.example.springsecurityjwt.entity.Users;
-import com.example.springsecurityjwt.repo.UsersRepo;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 @RestController
@@ -32,4 +24,5 @@ public class HomeController {
     public CsrfToken csrfToken(HttpServletRequest request){
         return (CsrfToken) request.getAttribute("_csrf");
     }
+
 }
